6af536745b0cf43d3e632469e53a62feb51c6623
Fernando <fst_96@hotmail.com>
2022-03-03 17:09:51 -0600

    anadiendo floor en match

--- 03-median-of-two-sorted-array.js
+++ 03-median-of-two-sorted-array.js
@@ -4,12 +4,14 @@ function findMedianSortedArrays(nums1, nums2) {
     arrCompleto.push(nums1, nums2);
     let arrayOrdenado = arrCompleto.flat();
     arrayOrdenado = arrayOrdenado.sort(function (a, b) { return a - b; });
+    console.log(arrayOrdenado);
     let par = null;
     let parPlus = null;
     let resultadoPar = null;
     let impar = null;
     if (arrayOrdenado.length % 2 === 0) {
         par = (arrayOrdenado.length - 1) / 2;
+        console.log(par, 'valor par');
         par = Math.floor(par);
         let parPosicion = par;
         par = +arrayOrdenado[par];
@@ -23,10 +25,14 @@ function findMedianSortedArrays(nums1, nums2) {
             return +arrayOrdenado[0];
         }
         impar = +arrayOrdenado[arrayOrdenado.length - 1];
+        console.log(impar, 'valor del impar');
+        let imparPosicion = Math.floor(impar / 2);
         console.log('es impar');
-        return Math.ceil(impar / 2);
+        console.log(Math.floor(impar / 2), 'Esto me daria la posicion del valor de enmedio');
+        console.log(+arrayOrdenado[imparPosicion], 'resultado de todo');
+        return +arrayOrdenado[imparPosicion];
     }
 }
 ;
-console.log(findMedianSortedArrays([1, 3], [2]));
+console.log(findMedianSortedArrays([1, 2], [3, 4, 5]));
 const numeros = [1, 3, 0];
--- 03-median-of-two-sorted-array.ts
+++ 03-median-of-two-sorted-array.ts
@@ -5,7 +5,8 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 
     let arrayOrdenado = arrCompleto.flat() ;
     arrayOrdenado = arrayOrdenado.sort(function(a:any, b:any){return a - b});
-
+    console.log(arrayOrdenado);
+    
 
     let par:number | null = null;
     let parPlus:number | null = null;
@@ -16,6 +17,8 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     if(arrayOrdenado.length %2 === 0){
         
         par = (arrayOrdenado.length - 1) / 2;
+        console.log(par,'valor par');
+        
         par = Math.floor(par);
         let parPosicion = par ;
         par = +arrayOrdenado[par];
@@ -32,13 +35,21 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
             return +arrayOrdenado[0];
         }
         impar = +arrayOrdenado[arrayOrdenado.length - 1];
+        console.log(impar,'valor del impar');
+        let imparPosicion = Math.floor(impar/2);
         console.log('es impar');
-        return Math.ceil(impar / 2);
+        console.log(Math.floor(impar/2), 'Esto me daria la posicion del valor de enmedio');
+        
+        console.log( +arrayOrdenado[imparPosicion],'resultado de todo');
+        
+        return +arrayOrdenado[imparPosicion];
+
+        // TODO EN impar tiene que regresar el valor de enmedio
         
     }
 };
 
-console.log(findMedianSortedArrays([1,3],[2]));
+console.log(findMedianSortedArrays([1,2],[3,4,5]));
 
 
 const numeros = [1,3,0];
